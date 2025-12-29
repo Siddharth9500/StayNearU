7b92b3d157456180a0097c1acff6342db87284f1
Siddharth9500 <tardesiddharth9500@gmail.com>
2025-12-29 12:55:23 +0530

    Add src folder with entry point main.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        {/* Your app routes and components will go here */}
+      </div>
+    </Router>
+  )
+}
