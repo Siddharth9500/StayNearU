fa520bc37aa96f678f84b00e8fafaeae6e81a463
Siddharth9500 <tardesiddharth9500@gmail.com>
2025-12-29 16:00:35 +0530

    Install all missing dependencies and fix configs

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+    "./Pages/**/*.{js,jsx}",
+    "./Components/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
